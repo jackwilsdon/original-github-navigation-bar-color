cabf659b8ddc12ecb68fb57f72753fcb7fee9c2d
Jack Wilsdon <jack.wilsdon@gmail.com>
2017-02-17 12:58:36 +0000

    Add Userscript build script

---
+++ userscript/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp'),
+      fs = require('fs'),
+      { version } = require('./package.json'),
+      cssmin = require('gulp-cssmin'),
+      css2js = require('gulp-css2js'),
+      rename = require('gulp-rename');
+
+gulp.task('build', () => {
+  const header = fs
+    .readFileSync('./header.js', 'utf8')
+    .replace(/^(\/\/\s*@version\s*) .*$/m, `$1 ${version}`)
+
+  return gulp.src('../extension/fix-navigation-color.css')
+      .pipe(cssmin())
+      .pipe(css2js({
+          prefix: header + 'GM_addStyle("',
+          suffix: '");'
+      }))
+      .pipe(rename('userscript.js'))
+      .pipe(gulp.dest('.'))
+});
+
+gulp.task('default', [ 'build' ])
---
+++ userscript/header.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Original GitHub Navigation Bar Color
+// @namespace    http://jackwilsdon.me
+// @version      <filled in by gulp>
+// @description  Reverts the GitHub navigation bar back to it's original color.
+// @author       Jack Wilsdon <jack.wilsdon@gmail.com>
+// @match        *://github.com/*
+// @match        *://*.github.com/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
